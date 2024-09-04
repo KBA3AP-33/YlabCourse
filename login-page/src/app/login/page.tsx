6e0f944c4754981239cf5106766dda9d1c7bb08c
KBA3AP-33 <113384477+KBA3AP-33@users.noreply.github.com>
2024-09-04 23:37:09 +0300

    Update page.tsx
    fix

--- login-page/src/app/login/page.tsx
+++ login-page/src/app/login/page.tsx
@@ -1,9 +1,12 @@
 import { Login } from "@/app/components/authentication/login";
+import { Suspense } from 'react';
 
 export default function Page() {
     return (
         <main className="h-screen flex justify-center items-center dark:bg-[#111827] dark:text-white">
-            <Login/>
+            <Suspense>
+                <Login/>
+            </Suspense>
         </main>
     )
 }
